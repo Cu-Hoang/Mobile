ed5faa7a3440982b86df0f9af0c21ff989bd08af
MonVerloren <93923049+MonVerloren@users.noreply.github.com>
2024-07-07 02:39:16 +0700

    Delete BE/src/routes/wishlist.js

--- BE/src/routes/wishlist.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const wishlistController = require('../controller/WishlistController');
-
-router.post('/add', wishlistController.add);
-router.delete('/remove/:productId', wishlistController.remove);
-router.get('/user/:userId', wishlistController.getUserWishlist);
-
-module.exports = router;
